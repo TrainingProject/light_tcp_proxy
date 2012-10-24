3a8edabd4acba8759d53bda521008a9a24df8ff0
gfree.wind@gmail.com <gfree.wind@gmail.com>
2012-10-24 23:13:46 +0800

    add raw socket files when run in user space

---
+++ main/ltp_raw_socket.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ltp_raw_socket.c
+ *
+ *    Description:  Use raw socket to send packet when run in the user space
+ *
+ *        Version:  1.0
+ *        Created:  10/24/2012 10:42:53 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Feng Gao
+ *          Email:  gfree.wind@gmail.com  
+ *
+ * =====================================================================================
+ */
+
+static int ltp_raw_socket = -1;
+
+
+
